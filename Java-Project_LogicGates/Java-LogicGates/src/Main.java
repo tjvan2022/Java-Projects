d1fbbc1a0cc6b623c034587496b1228692d4375c
tjvan2022 <edu.tjsaggu@gmail.com>
2022-09-03 13:22:26 -0700

    updated main

--- Java-Project_LogicGates/Java-LogicGates/src/Main.java
+++ Java-Project_LogicGates/Java-LogicGates/src/Main.java
@@ -1,2 +1,25 @@
+/*
+       Date: September 3, 2022
+       Description:
+       This java project is really interesting and an eye-catching project for engineering students.
+       You may be knowing that logic gates are implemented using boolean functions and are used to perform specific logical operations.
+       There are six basic logic gates which are: AND gate, OR gate, NOT gate, NAND gate, and NOR gate.
+       The NOT gate performs on a single input and all other gates can be performed with either two or three inputs.
+       In this system, you can give the logical inputs to the gates of your requirement and get the result in either 0 or 1.
+       There will be options to choose 2-input or 3-input in the gates except for the NOT gate that will take only a single input(0 or 1) from the user.
+ */
+
 public class Main {
+    public static void main (String [] args){
+        System.out.println("Hello World!");
+
+        logicMenu();
+
+    }
+
+    static void logicMenu()
+    {
+
+    }
 }
+
