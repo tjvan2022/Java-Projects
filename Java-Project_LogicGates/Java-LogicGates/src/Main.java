4fb6546d08165a30d9475fc641a029387f182a3a
tjvan2022 <edu.tjsaggu@gmail.com>
2022-09-04 20:51:09 -0700

    update

--- Java-Project_LogicGates/Java-LogicGates/src/Main.java
+++ Java-Project_LogicGates/Java-LogicGates/src/Main.java
@@ -9,9 +9,12 @@
        There will be options to choose 2-input or 3-input in the gates except for the NOT gate that will take only a single input(0 or 1) from the user.
  */
 
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class Main {
     public static void main (String [] args){
-        System.out.println("Hello World!");
+        //System.out.println("Hello World!");
 
         logicMenu();
 
@@ -19,7 +22,77 @@ public class Main {
 
     static void logicMenu()
     {
-        
+        Scanner readUserInputObject = new Scanner(System.in);
+        boolean exitProgram = true;
+        int switchStatementInput = 0;
+
+
+        try {
+            while (exitProgram) {
+                // System.out.println("Please input a line");
+                // String line = readUserInputObject.nextLine();
+                // System.out.printf("User input was: %s%n", line);
+                System.out.println("");
+                System.out.println("Please select GATE function");
+                System.out.println("1. 2-Input AND Gate");
+                System.out.println("2. Exit");
+                String line = readUserInputObject.nextLine();
+                switchStatementInput = Integer.parseInt(line);
+
+
+                switch(switchStatementInput) {
+                    case 1:
+                        // code block
+                        break;
+                    case 2:
+                        exitProgram = false;
+                        break;
+                    default:
+                        // code block
+                }
+            }
+        } catch(IllegalStateException | NoSuchElementException e) {
+            // System.in has been closed
+            System.out.println("System.in was closed; exiting");
+        }
+
     }
+
+    static void twoInputAndGate()
+    {
+        Scanner readUserInputObject = new Scanner(System.in);
+        boolean exitProgram = true;
+        int switchStatementInput = 0;
+
+        try {
+            while (exitProgram) {
+                // System.out.println("Please input a line");
+                // String line = readUserInputObject.nextLine();
+                // System.out.printf("User input was: %s%n", line);
+                System.out.println("");
+                System.out.println("Please select GATE function");
+                System.out.println("1. 2-Input AND Gate");
+                System.out.println("2. Exit");
+                String line = readUserInputObject.nextLine();
+                switchStatementInput = Integer.parseInt(line);
+
+
+                switch(switchStatementInput) {
+                    case 1:
+                        // code block
+                        break;
+                    case 2:
+                        exitProgram = false;
+                        break;
+                    default:
+                        // code block
+                }
+            }
+        } catch(IllegalStateException | NoSuchElementException e) {
+            // System.in has been closed
+            System.out.println("System.in was closed; exiting");
+        }
+    }
+
 }
 
